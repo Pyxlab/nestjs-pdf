67cab2b9fcd9756a2da036168aef05d7f4ba3890
Walaff Fernandes <walaffonofre@icloud.com>
2023-12-07 07:59:05 -0300

    fix: update filename generation in defaultCreateOptions function

--- lib/pdf.default.ts
+++ lib/pdf.default.ts
@@ -1,7 +1,6 @@
 import { join } from 'path';
 import { tmpdir } from 'os';
-import { randomUUID } from 'crypto';
 
 export const defaultCreateOptions = () => ({
-    filename: join(tmpdir(), `html-pdf-${process.pid}-${randomUUID()}.pdf`),
+    filename: join(tmpdir(), `html-pdf-${process.pid}-${Date.now()}.pdf`),
 });
