d5b117c6c616025f4e07e695845e98d1e8239011
Walaff Fernandes <walaffonofre@icloud.com>
2023-12-07 00:12:38 -0300

    feat: update defaultCreateOptions to include a unique filename using randomUUID()

--- lib/pdf.default.ts
+++ lib/pdf.default.ts
@@ -1,6 +1,7 @@
 import { join } from 'path';
 import { tmpdir } from 'os';
+import { randomUUID } from 'crypto';
 
-export const defaultCreateOptions = {
-    filename: join(tmpdir(), `html-pdf-${process.pid}.pdf`),
-};
+export const defaultCreateOptions = () => ({
+    filename: join(tmpdir(), `html-pdf-${process.pid}-${randomUUID()}.pdf`),
+});
